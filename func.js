1b2817d0e4dedbe3c526b2cab50b9706c080d3f5
skjaguar2030 <seifira21@protonmail.com>
2022-10-23 00:32:59 +0200

    Initial commit debugged

--- func.js
+++ func.js
@@ -9,9 +9,6 @@ form.addEventListener( "submit", e => { e.preventDefault(); checkInputs() });
 
 function checkInputs() {
 
-    console.log(passwordValue)
-
-
     const usernameValue = username.value.trim();
     const emailValue = email.value.trim();
     const passwordValue = password.value.trim();
